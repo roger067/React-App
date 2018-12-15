6b0c76c63295a5e9e7611d01cbf6c64d4bce7628
Rogerio Moura <rosestolato.rogerio@gmail.com>
2018-12-15 00:30:06 -0200

    Tabela com json

--- htmltabela/js/index.js
+++ htmltabela/js/index.js
@@ -1,26 +1,13 @@
-
-
 $.getJSON("js/data.json", function(data) {
-  
-    var items= [];
-    $.each(data, function(name, adress, city, region, country, birthday){
-        items.push("<td id ='"+ name +"'></td> <td id ='"+ adress +"'></td> <td id ='"+ city +"'></td> <td id ='"+ region +"'></td> <td id ='"+ country +"'></td> <td id ='"+ birthday +"'></td>");
-        
-        $("<tr/>",{
-            html: items.join("")
-        }).appendTo("tbody");
-        });
-    
-    });
-    /*for(var i=0;i< json.length; i++){
-        var html = "<tr>";
-        html +="<td>"+json[i].name+"</td>";
-        html +="<td>"+json[i].adress+"</td>";
-        html +="<td>"+json[i].city+"</td>";
-        html +="<td>"+json[i].region+"</td>";
-        html +="<td>"+json[i].country+"</td>";
-        html +="<td>"+json[i].birthday+"</td>";
-        html +="</tr>";
+    var html = "<tr>";
+    for (var i = 0; i < data.length; i++) {
+        html += "<td>" + data[i].name + "</td>";
+        html += "<td>" + data[i].address + "</td>";
+        html += "<td>" + data[i].city + "</td>";
+        html += "<td>" + data[i].region + "</td>";
+        html += "<td>" + data[i].country + "</td>";
+        html += "<td>" + data[i].birthday + "</td>";
+        html += "</tr>";
     }
-  
-    document.getElementById("table-hover").innerHTML = html;*/
+    document.getElementById('table-js').innerHTML = html;
+});
