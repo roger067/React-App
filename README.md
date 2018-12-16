86112262e7fa877cd38c38fbb06329024d728c12
Rogerio Moura <rosestolato.rogerio@gmail.com>
2018-12-16 20:44:01 -0200

    read.me

--- README.md
+++ README.md
@@ -3,13 +3,5 @@ Projeto feito com o intuito de aprender funcionalidades do React
 
 ****Descrição de como criar o projeto****
 
-Rodar em servidor local, porque o CORS não deixa acessar endereços externos.
-
-
-### `npm start`
-
-Executa o aplicativo no modo de desenvolvimento. 
+Abra o console na pasta raíz e digite o commando npm start que executará o aplicativo no modo de desenvolvimento. 
 Abra [http: // localhost: 3000] (http: // localhost: 3000) para visualizá-lo no navegador.
-
-A página será recarregada se você fizer edições. 
-Você também verá quaisquer erros de lint no console.
---
+++ nata/src/components/data-table/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function CoinTable(props) {
+  return (
+    <div className="scroll-table">
+      <table className="table-fill">
+        <thead>
+          <tr>
+            <th className="text-left">Nome</th>
+            <th className="text-left">Endereço</th>
+            <th className="text-left">Cidade</th>
+            <th className="text-left">Região</th>
+            <th className="text-left">País</th>
+            <th className="text-left">Aniversário</th>
+          </tr>
+        </thead>
+        <tbody className="table-hover">
+          {
+            props.data.map(row => (
+              <tr>
+                <td>{row.name}</td>
+                <td>{row.address}</td>
+                <td>{row.city}</td>
+                <td>{row.region}</td>
+                <td>{row.country}</td>
+                <td>{row.birthday}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
