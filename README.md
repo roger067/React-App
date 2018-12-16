953f3767ed53cd9c9e490ecc10c9a4806b25d7f6
Rogerio Moura <rosestolato.rogerio@gmail.com>
2018-12-16 20:38:02 -0200

    Finalização e Ajustes

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 Projeto feito com o intuito de aprender funcionalidades do React
 
 ****Descrição de como criar o projeto****
+
+Rodar em servidor local, porque o CORS não deixa acessar endereços externos.
+
+
+### `npm start`
+
+Executa o aplicativo no modo de desenvolvimento. 
+Abra [http: // localhost: 3000] (http: // localhost: 3000) para visualizá-lo no navegador.
+
+A página será recarregada se você fizer edições. 
+Você também verá quaisquer erros de lint no console.
--- nata/src/App.js
+++ nata/src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CoinTable from './components/data-table'
 
-class App extends Component {
+import data from './data/data.json'
+
+import './app.css'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: data,
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <CoinTable
+          data={this.state.data} 
+        />
+    )
   }
 }
 
-export default App;
+export default App
